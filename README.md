9ee7ba3a868187b29627e564cf59e71942d89128
Andy Lin <aero00515@gmail.com>
2020-05-17 15:09:32 +0800

    init auth from l3 hello-cache

---
+++ README.md
@@ -0,0 +1,19 @@
+# Lesson 3 - Cache War
+
+Show cache's magic.  
+With advanced promise reuse skill.
+
+# For VSCode User
+
+Please install following plugins:
+
+1. ESLint
+2. REST Client
+3. (Optional) Prettier
+
+# How to start
+
+```
+npm install
+npm start
+```
---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const HttpStatus = require('http-status-codes');
+const { nanoid } = require('nanoid');
+const { response } = require('./generator');
+const { APP, HTTP } = require('./constants');
+const { timer } = require('./services');
+const { helloRouter } = require('./router');
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/getHello', helloRouter);
+
+app.post('/', async (req, res) => {
+  // Get request datas
+  const reqId = req.header(HTTP.HEADER.X_REQUSET_ID) || nanoid();
+  const body = req.body;
+
+  const sleepSec = Math.random() * 10;
+  const sleepMilliSec = Math.round(sleepSec * 1000);
+  console.log('POST Start: ', {
+    reqId,
+    body,
+    sleepMilliSec,
+  });
+
+  // Do sleep for simulating gard work job
+  await timer.sleep(sleepMilliSec);
+
+  console.log('POST End: ', {
+    reqId,
+    body,
+    sleepMilliSec,
+  });
+
+  // Response
+  const apiResponse = response.getResponse({
+    message: '// TODO: meake your own success message',
+    params: {
+      reqId,
+      ...body,
+    },
+  });
+  res.status(HttpStatus.CREATED).json(apiResponse);
+});
+
+app.listen(APP.PORT, () => {
+  console.log(`Listen on ${APP.PORT}`);
+});
